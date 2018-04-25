1abe8835a61f667d31ad5191da32c317fdc4c100
matukai <matt.tanji@gmail.com>
2018-04-24 15:00:38 -1000

    intermediary

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-  return res.render('USER BACKEND ROUTE')
+  return res.render('ARTIST BACKEND ROUTE')
 })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/users');
+const artistRoutes = require('./routes/artists');
+const path = require('path');
 
 //CONSTANTS
 const PORT = process.env.PORT || 8080;
@@ -9,7 +11,12 @@ const PORT = process.env.PORT || 8080;
 //APPLICATIONS
 app.use(express.static('public'));
 app.use('/users', userRoutes);
-
+app.use('/artists', artistRoutes);
+// app.use(
+//   express.static(path.join(__dirname, 'public'), {
+//       etag: false
+//   })
+// );
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
